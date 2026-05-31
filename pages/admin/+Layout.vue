23342c7dc789cdfc25631dfb282817a699a72c0a
tufensama <78685078+tufensama@users.noreply.github.com>
2026-05-31 19:38:49 +0800

    Update +Layout.vue

--- pages/admin/+Layout.vue
+++ pages/admin/+Layout.vue
@@ -127,7 +127,7 @@
         <div class="p-4 border-t border-base-300 mt-auto space-y-2">
           <AppButton variant="outline" block @click="handleSignOut">退出登录</AppButton>
           <div class="flex items-center justify-between text-xs text-base-content/50 px-2">
-            <a class="cursor-default" target="_blank" href="https://github.com/34892002/edgeKey">edgeKey</a>
+            <a class="cursor-default" target="_blank" href="https://">edgeKey</a>
             <div class="">
               <button class="hover:text-primary transition-colors mr-2" @click="checkUpdate" :title="updateTip" >
                 v{{ appVersion }}-{{ gitHash }}
